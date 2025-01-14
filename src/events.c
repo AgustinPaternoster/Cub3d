2087a4675155e83581a2c4c17193ec7e2abea688
mgimon <m.gimoncaballero@gmail.com>
2025-01-14 19:14:15 +0100

    Pequeno ajuste - velocidad modificable

--- src/events.c
+++ src/events.c
@@ -6,7 +6,7 @@
 /*   By: mgimon-c <mgimon-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 17:05:57 by apaterno          #+#    #+#             */
-/*   Updated: 2025/01/07 03:54:34 by mgimon-c         ###   ########.fr       */
+/*   Updated: 2025/01/14 19:11:21 by mgimon-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,18 @@ static void move_player(t_game *game, int keycode)
 		if (keycode == XK_w && game->player->pos_y > PLAYERSIZE + 1)
 		{	
 			//if (game->player->pos_y > PLAYERSIZE + 1)
-				game->player->accum_y -= game->player->dy;
-				game->player->accum_x += game->player->dx;
+				game->player->accum_y -= game->player->dy * VELOCITY;
+				game->player->accum_x += game->player->dx * VELOCITY;
 				game->player->pos_x = round(game->player->accum_x);
 				game->player->pos_y = round(game->player->accum_y);	
 		}
 		if (keycode == XK_s &&
 			game->player->pos_y < (game->map->sizey * GRIDSIZE) - PLAYERSIZE * 2) 
 		{
-				game->player->accum_y += game->player->dy;
-				game->player->accum_x -= game->player->dx;
+				game->player->accum_y += game->player->dy * VELOCITY;
+				game->player->accum_x -= game->player->dx * VELOCITY;
 				game->player->pos_x = round(game->player->accum_x);
-				game->player->pos_y = round(game->player->accum_y);	
+				game->player->pos_y = round(game->player->accum_y);
 		}
 		render_frame(game);
 }
@@ -40,17 +40,18 @@ static void rotate_player(t_game *game, int keycode)
 	if (keycode == XK_d)
 	{
 		if (game->player->direction == 0)
-			game->player->direction = 360 - 1;
+			game->player->direction = 360 - VELOCITY;
 		else
-			game->player->direction--;
+			game->player->direction -= VELOCITY;
 	}
 	if (keycode == XK_a)
 	{
 		if (game->player->direction == 360)
 			game->player->direction = 0;
 		else
-			game->player->direction++;
+			game->player->direction += VELOCITY;
 	}
+	game->player->direction %= 360; // No + de 360
 	calculate_delta(game);	
 	render_frame(game);
 }

5dbee332ca7f3ec50c69451519d6c74620246a6f
mgimon-asus <m.gimoncaballero@gmail.com>
2025-02-21 04:26:10 +0100

    Parsing final

--- src/resources.c
+++ src/resources.c
@@ -48,9 +48,11 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-void get_cub(t_game *game, char *filename) {
+void get_cub(t_game *game, char *filename)
+{
     game->map->fd = open(filename, O_RDONLY);
-    if (game->map->fd < 0) {
+    if (game->map->fd < 0)
+    {
         perror("Failed to open file");
         return;
     }
@@ -59,13 +61,15 @@ void get_cub(t_game *game, char *filename) {
     size_t count = 0;
     size_t capacity = 10;
     game->map->cub = malloc(sizeof(char *) * capacity);
-    if (!game->map->cub) {
+    if (!game->map->cub)
+    {
         perror("Memory allocation failed");
         close(game->map->fd);
         return;
     }
 
-    while ((line = get_next_line(game->map->fd))) {
+    while ((line = get_next_line(game->map->fd)))
+    {
         if (count >= capacity) {
             capacity *= 2;
             char **new_cub = realloc(game->map->cub, sizeof(char *) * capacity);
@@ -92,14 +96,14 @@ int get_hex_from_cubline(char *line)
     while (line[i] == ' ' || line[i] == '\t')
         i++;
     if ((line[i] != 'C' && line[i] != 'F') || line[i + 1] != ' ')
-        return 0xFFFFFF;
+        return -1;
     i += 2;
     while (line[i] && ci < 3)
     {
         while (line[i] == ' ' || line[i] == '\t')
             i++;
         if (!isdigit(line[i]))
-            return 0xFFFFFF;
+            return -1;
         colors[ci] = 0;
         while (isdigit(line[i]))
         {
@@ -107,13 +111,13 @@ int get_hex_from_cubline(char *line)
             i++;
         }
         if (colors[ci] < 0 || colors[ci] > 255)
-            return 0xFFFFFF;
+            return -1;
         if (ci < 2)
         {
             while (line[i] == ' ' || line[i] == '\t')
                 i++;
             if (line[i] != ',')
-                return 0xFFFFFF;
+                return -1;
             i++;
         }
         ci++;
@@ -121,9 +125,10 @@ int get_hex_from_cubline(char *line)
     while (line[i] == ' ' || line[i] == '\t' || line[i] == '\n' || line[i] == '\r')
         i++;
     if (line[i] != '\0')
-        return 0xFFFFFF;
+        return -1;
     return (colors[0] << 16) | (colors[1] << 8) | colors[2];
 }
+
 
 int get_full_width(char **matrix)
 {
@@ -145,10 +150,10 @@ int get_full_width(char **matrix)
     return (width);
 }
 
-void init_resources(t_game *game, char *filename)
+/*void init_resources(t_game *game, char *filename)
 {
-    game->map->ceiling = 0xFFFFFF;
-    game->map->floor = 0xFFFFFF;
+    game->map->ceiling = -1;
+    game->map->floor = -1;
     get_cub(game, filename);
     for (int i = 0; game->map->cub[i] != NULL; i++)
     {
@@ -160,6 +165,50 @@ void init_resources(t_game *game, char *filename)
             else
                 game->map->floor = color;
         }
+    }
+    printf("techo: %d\n", game->map->ceiling);
+    printf("suelo: %d\n", game->map->floor);
+    if (game->map->ceiling == -1 || game->map->floor == -1)
+    {
+        printline_fd(2, "Error: One or more colors not found\n");
+        exit(1);
+    }
+    game->map->textures = malloc(sizeof(t_texture));
+    game->map->sizey = get_full_height(game->map->matrix);
+    game->map->sizex = get_full_width(game->map->matrix);
+    set_player_pos(game);
+    get_texture("NO", game);
+    get_texture("EA", game);
+    get_texture("WE", game);
+    get_texture("SO", game);
+}*/
+
+void init_resources(t_game *game, char *filename)
+{
+    game->map->ceiling = -1;
+    game->map->floor = -1;
+    get_cub(game, filename);
+    for (int i = 0; game->map->cub[i] != NULL; i++)
+    {
+        char *line = game->map->cub[i];
+        while (*line == ' ' || *line == '\t')
+            line++;
+        
+        if (line[0] == 'C' || line[0] == 'F')
+        {
+            int color = get_hex_from_cubline(line);
+            if (line[0] == 'C')
+                game->map->ceiling = color;
+            else
+                game->map->floor = color;
+        }
+    }
+    printf("techo: %d\n", game->map->ceiling);
+    printf("suelo: %d\n", game->map->floor);
+    if (game->map->ceiling == -1 || game->map->floor == -1)
+    {
+        printline_fd(2, "Error: One or more colors not found\n");
+        exit(1);
     }
     game->map->textures = malloc(sizeof(t_texture));
     game->map->sizey = get_full_height(game->map->matrix);
